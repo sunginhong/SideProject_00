143478ed1b78a9d27d221b36d6a349b718cfd00e
sunginhong <nicehsi@naver.com>
2018-12-20 19:05:42 +0900

    test_181219_5

--- app/src/main/java/com/example/sunginhong/sideproject_00/Main_BottomFunc.kt
+++ app/src/main/java/com/example/sunginhong/sideproject_00/Main_BottomFunc.kt
@@ -1,20 +1,10 @@
 package com.example.sunginhong.sideproject_00
 
 import android.content.Context
-import android.util.AttributeSet
 import android.util.Log
-import android.widget.RelativeLayout
-import kotlinx.android.synthetic.main.main_bottom_actionbar.view.*
 
-
-class Main_BottomFunc : RelativeLayout {
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init(context)
-    }
-
-    private fun init(context: Context) {
-        Log.d("ssssss", "ssssssss"+bottomMenu_btn_alarm)
+class Main_BottomFunc (val context: Context) {
+    init {
+        Log.d("ssssss", "ssssssss")
     }
 }
