e4e2b563ffb37ffbf7b2aba1c8c5a2691da7b545
sunginhong <nicehsi@naver.com>
2018-12-26 13:52:10 +0900

    test_181226_1

--- app/src/main/java/com/example/sunginhong/sideproject_00/Main_BottomFunc.kt
+++ app/src/main/java/com/example/sunginhong/sideproject_00/Main_BottomFunc.kt
@@ -3,27 +3,43 @@ package com.example.sunginhong.sideproject_00
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.main_bottom_actionbar.view.*
 import java.util.*
 import kotlin.concurrent.timerTask
 
+
 class Main_BottomFunc : RelativeLayout {
+    companion object {
+        val arrayBtmButton = arrayListOf<ImageButton>()
+        var BottomMenu_item_Contain: LinearLayout? = null
+
+    }
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val timer = Timer()
         timer.schedule(timerTask {
-            bottomBtnSet()
+            bottomBtn_init()
         }, 100)
     }
 
-    private fun bottomBtnSet () {
+    private fun bottomBtn_init () {
         // ButtonSetting
-        bottomMenu_btn_write.setOnClickListener { Log.d("ssssss", "sssss"+bottomMenu_btn_write) }
-        bottomMenu_btn_home.setOnClickListener { Log.d("ssssss", "sssss"+bottomMenu_btn_home) }
-        bottomMenu_btn_search.setOnClickListener { Log.d("ssssss", "sssss"+bottomMenu_btn_search) }
-        bottomMenu_btn_my.setOnClickListener { Log.d("ssssss", "sssss"+bottomMenu_btn_my) }
-        bottomMenu_btn_alarm.setOnClickListener { Log.d("ssssss", "sssss"+bottomMenu_btn_alarm) }
+        BottomMenu_item_Contain = bottomMenu_item_Contain
+
+        arrayBtmButton.add(bottomMenu_btn_write)
+        arrayBtmButton.add(bottomMenu_btn_home)
+        arrayBtmButton.add(bottomMenu_btn_search)
+        arrayBtmButton.add(bottomMenu_btn_my)
+        arrayBtmButton.add(bottomMenu_btn_alarm)
+
+        for (i in 0 until arrayBtmButton.size) {
+            arrayBtmButton[i].setOnClickListener {
+                Log.d("ssssss", ""+i)
+            }
+        }
     }
 }
