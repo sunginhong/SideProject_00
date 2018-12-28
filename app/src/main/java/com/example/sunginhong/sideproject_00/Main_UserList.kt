993553eff2cf4a5d32db855c29a4d77d62771d5b
sunginhong <nicehsi@naver.com>
2018-12-28 14:20:59 +0900

    test_181228_2

--- app/src/main/java/com/example/sunginhong/sideproject_00/Main_UserList.kt
+++ app/src/main/java/com/example/sunginhong/sideproject_00/Main_UserList.kt
@@ -2,14 +2,13 @@ package com.example.sunginhong.sideproject_00
 
 val MainUserList = arrayListOf<Main_User>(
     Main_User("A","hong@naver.com","https://s3.amazonaws.com/appsdeveloperblog/micky.gif"),
-    Main_User("B","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("C","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("D","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("E","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("F","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("G","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("H","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("I","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("J","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png"),
-    Main_User("K","hong@naver.com","https://cdn.dribbble.com/users/14268/screenshots/5593802/joy-fm_2x.png")
+    Main_User("B","hong@naver.com","https://cdn.dribbble.com/users/740218/screenshots/5082891/dribbble_diver.jpg"),
+    Main_User("C","hong@naver.com","https://cdn.dribbble.com/users/740218/screenshots/5693867/jenga_dribbble.jpg"),
+    Main_User("D","hong@naver.com","https://cdn.dribbble.com/users/783963/screenshots/3944886/5.gif"),
+    Main_User("E","hong@naver.com","https://cdn.dribbble.com/users/798921/screenshots/5392043/jitw_dribbble_2x.png"),
+    Main_User("F","hong@naver.com","https://cdn.dribbble.com/users/2066522/screenshots/4840487/800600.png"),
+    Main_User("G","hong@naver.com","https://cdn.dribbble.com/users/1137911/screenshots/4231394/______.png"),
+    Main_User("H","hong@naver.com","https://cdn.dribbble.com/users/509505/screenshots/4224975/6.jpg"),
+    Main_User("I","hong@naver.com","https://cdn.dribbble.com/users/1217289/screenshots/5334929/sns_06_2x.jpg"),
+    Main_User("J","hong@naver.com","https://cdn.dribbble.com/users/1869594/screenshots/5290754/0922dribble.jpg")
 )
